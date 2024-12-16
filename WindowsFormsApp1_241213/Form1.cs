da643a5c1ac9ce4f76f12ea436214acffc4670fe
dseok2805 <eoierr@gmail.com>
2024-12-16 12:04:43 +0900

    실습준비완료

--- WindowsFormsApp1_241213/Form1.cs
+++ WindowsFormsApp1_241213/Form1.cs
@@ -18,14 +18,24 @@ namespace WindowsFormsApp1_241213
             // Program.cs에서 윈도우에서 볼 수 있도록 창을 띄워줌
             // MessageBox.Show("하하하하하핳하"); // 메세지박스를 출력할 수 있음
 
-            // 텍스트 박스에 메세지 출력
-            textBox1_print.Text = "이것은\r\n멀티라인\r\n텍스트박스\r\n인데요.";
-            // \r\n : 개행문자
-            int a = 0; // int 형 a 변수를 선언함
-            a = 47; // a 변수를 변경함
+            //// \r\n : 개행문자
+            //int a = 0; // int 형 a 변수를 선언함
+            //a = 47; // a 변수를 변경함
 
-            string msg = "안녕";
-            msg = "dseok";
+            //string msg = "안녕";
+            //msg = "dseok";
+
+            // 텍스트 박스에 메세지 출력
+            textBox1_print.Text = "어떤 결과가 나올까??";
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            // Form1.cs에서 이 함수를 지우고
+            // Design.cs 파일에서
+            // this.label1.Click += new System.EventHandler(this.label1_Click) 빨간 부분을 주석처리함
         }
     }
 }
