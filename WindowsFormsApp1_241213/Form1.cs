5035630429ab6e584c77f650405d25700353a4fb
dseok2805 <eoierr@gmail.com>
2024-12-17 13:37:40 +0900

    241217 문자열 실습2 Substring 활용

--- WindowsFormsApp1_241213/Form1.cs
+++ WindowsFormsApp1_241213/Form1.cs
@@ -16,25 +16,17 @@ namespace WindowsFormsApp1_241213
         public Form1()
         {
             InitializeComponent();
-            // Ctrl + K + C
-            // Ctrl + K + U 한꺼번에 주석 처리 가능
-            // # region, #endregion 활용해 묶을 수 있음
             string keyword = "멈추지 않는 한 얼마나 천천히 가는지는 중요하지 않다. -공자";
-            string str1 = keyword.Remove(keyword.IndexOf("-")); // 멈추지 않는 한 얼마나 천천히 가는지는 중요하지 않다. 
+            string word = keyword.Substring(keyword.IndexOf("-"),3); // -공자를 찾기 위해 -부터 3글자를 Substring()
+            string str1 = keyword.Remove(keyword.IndexOf(word)); // 위에서 Substring()한 word를 활용해 Remove()
+            // 멈추지 않는 한 얼마나 천천히 가는지는 중요하지 않다. 
 
             // IndexOf() 또는 LastIndexOf()를 사용하여 단어를 검색하고,
             // SubString() 또는 Remove() 그리고 Split()을 사용하여 “얼마나”, “천천히”, “가는지” 세 개 단어로 나누어 배열의 요소에 각각 저장
-            // 1. 얼마나 천천히 가는지는 으로 빼내기
-            string str2 = keyword.Remove(0, keyword.IndexOf("얼마나")); // 얼마나 천천히 가는지는 중요하지 않다. -공자
-            str2 = str2.Remove(str2.IndexOf("중요")); // 얼마나 천천히 가는지는 
-            str2 = str2.Remove(str2.LastIndexOf("는")); // 얼마나 천천히 가는지
-            string[] splitArray = str2.Split(' '); // 공백을 기준으로 3칸의 배열이 생성될 것임
-            // 기존 배열에 하나씩 할당
             string[] split_str2 = new string[3];
-            split_str2[0] = splitArray[0]; // 얼마나
-            split_str2[1] = splitArray[1]; // 천천히
-            split_str2[2] = splitArray[2]; // 가는지
-
+            split_str2[0] = keyword.Substring(keyword.IndexOf("얼마나"), 3); // 얼마나
+            split_str2[1] = keyword.Substring(keyword.IndexOf("천천히"), 3); // 천천히
+            split_str2[2] = keyword.Substring(keyword.IndexOf("가는지"), 3); // 가는지
 
             // “.”과 “-”를 제거하고, 모든 공백 문자를 “,”로 바꾸기
             string str3 = keyword.Remove(keyword.IndexOf("."), 1); // 멈추지 않는 한 얼마나 천천히 가는지는 중요하지 않다 -공자
@@ -47,6 +39,5 @@ namespace WindowsFormsApp1_241213
             textBox1_print.Text += split_str2[2] + "\r\n";
             textBox1_print.Text += str3;
         }
-
     }
 }
