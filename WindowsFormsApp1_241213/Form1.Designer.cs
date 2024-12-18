2058cbed5c9fbb0f985c2f76355581cdb1b2a842
dseok2805 <eoierr@gmail.com>
2024-12-18 13:16:20 +0900

    241218 사용자 입력 기본 설정완료

--- WindowsFormsApp1_241213/Form1.Designer.cs
+++ WindowsFormsApp1_241213/Form1.Designer.cs
@@ -28,34 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1_print = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.button_input = new System.Windows.Forms.Button();
+            this.textBox_result = new System.Windows.Forms.TextBox();
+            textBox_input = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // textBox1_print
+            // textBox_input
             // 
-            this.textBox1_print.Location = new System.Drawing.Point(3, 32);
-            this.textBox1_print.Multiline = true;
-            this.textBox1_print.Name = "textBox1_print";
-            this.textBox1_print.Size = new System.Drawing.Size(793, 414);
-            this.textBox1_print.TabIndex = 0;
+            textBox_input.Location = new System.Drawing.Point(5, 6);
+            textBox_input.Name = "textBox_input";
+            textBox_input.Size = new System.Drawing.Size(622, 21);
+            textBox_input.TabIndex = 0;
             // 
-            // label1
+            // button_input
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 12);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Result : ";
+            this.button_input.Location = new System.Drawing.Point(642, 6);
+            this.button_input.Name = "button_input";
+            this.button_input.Size = new System.Drawing.Size(146, 23);
+            this.button_input.TabIndex = 1;
+            this.button_input.Text = "Input";
+            this.button_input.UseVisualStyleBackColor = true;
+            this.button_input.Click += new System.EventHandler(this.button_input_Click);
+            // 
+            // textBox_result
+            // 
+            this.textBox_result.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox_result.Location = new System.Drawing.Point(5, 33);
+            this.textBox_result.Multiline = true;
+            this.textBox_result.Name = "textBox_result";
+            this.textBox_result.ReadOnly = true;
+            this.textBox_result.Size = new System.Drawing.Size(783, 405);
+            this.textBox_result.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1_print);
+            this.Controls.Add(this.textBox_result);
+            this.Controls.Add(this.button_input);
+            this.Controls.Add(textBox_input);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -65,8 +77,9 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1_print;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_input;
+        private System.Windows.Forms.TextBox textBox_result;
+        private System.Windows.Forms.TextBox textBox_input;
     }
 }
 
--- WindowsFormsApp1_241213/Form1.cs
+++ WindowsFormsApp1_241213/Form1.cs
@@ -13,62 +13,16 @@ namespace WindowsFormsApp1_241213
 {
     public partial class Form1 : Form
     {
-        Random randomObj = new Random();
         public Form1()
         {
             InitializeComponent();
-            string[] result_coin = new string[2];
-            result_coin = coin_top_bottom(true);
-            textBox1_print.Text += "결과1: " + result_coin[0] + " 동전 랜덤 수: " + result_coin[1] + "\r\n";
-            result_coin = coin_top_bottom(true);
-            textBox1_print.Text += "결과2: " + result_coin[0] + " 동전 랜덤 수: " + result_coin[1] + "\r\n";
-            result_coin = coin_top_bottom(false);
-            textBox1_print.Text += "결과3: " + result_coin[0] + " 동전 랜덤 수: " + result_coin[1] + "\r\n";
-            result_coin = coin_top_bottom(false);
-            textBox1_print.Text += "결과4: " + result_coin[0] + " 동전 랜덤 수: " + result_coin[1] + "\r\n";
-
         }
-        /*
-        1. 동전 던지기(앞 면 또는 뒷 면) 함수를 작성
-            1. 함수 이름을 적당하게 짓기
-            2. 출력 자료형은 bool
-            3. 입력은 bool형 1개
-            4. “난수 생성”을 검색하여 난수 생성 방법을 학습하고 int형 난수를 생성
-            5. 생성된 난수와 % 연산을 이용, 연산 결과로 0 또는 1만 값이 나오도록 작성
-            6. 입력 값과 연산 결과를 비교하여 둘이 같으면 true를 반환, 다르면 false를 반환
-                1. 1=true, 0=false라고 가정
-        2. 함수에 true 또는 false를 입력하고 반환된 결과에 따라 “승리” 또는 “패배”로 TextBox에 표시
-        */
-        string[] coin_top_bottom(bool pick)
+
+        private void button_input_Click(object sender, EventArgs e)
         {
-            int randomValue = randomObj.Next(0, 20); // 0~20 사이의 난수를 생성함
-            int result;
-            string[] result_array = new string[2];
-
-            // 난수를 0 또는 1로 변환
-            if (randomValue % 2 == 1) // 난수의 나머지가 1이면 즉, 홀수면
-            {
-                result = 1; // 1을 반환함
-            }
-            else // 난수의 나머지가 0이면 즉, 짝수면
-            {
-                result = 0; // 0을 반환함       
-            }
-
-            result_array[1] = randomValue.ToString();
-
-            // 사용자 입력과 동전 던지기를 비교하여 결과를 반환함
-            if (((pick == true) & (result == 1)) || ((pick == false) & (result == 0)))
-            {
-                result_array[0] = "승리";
-                return result_array;
-            }
-            else 
-            {
-                result_array[0] = "패배";
-                return result_array;
-            }
+            // textBox_input.Text의 문자열을
+            // textBox_result.Text로 복사
+            textBox_result.Text = textBox_input.Text;
         }
-
     }
 }
