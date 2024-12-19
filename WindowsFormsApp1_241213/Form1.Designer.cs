d4a792df250fa2f1d3c912b04e33c9f217b98e03
dseok2805 <eoierr@gmail.com>
2024-12-19 10:00:14 +0900

    241219 사용자 입력 동전던지기 실습

--- WindowsFormsApp1_241213/Form1.Designer.cs
+++ WindowsFormsApp1_241213/Form1.Designer.cs
@@ -30,21 +30,25 @@
         {
             this.button_input = new System.Windows.Forms.Button();
             this.textBox_result = new System.Windows.Forms.TextBox();
-            textBox_input = new System.Windows.Forms.TextBox();
+            this.radioButton_True = new System.Windows.Forms.RadioButton();
+            this.radioButton_False = new System.Windows.Forms.RadioButton();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.textBox_input = new System.Windows.Forms.TextBox();
+            this.groupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox_input
             // 
-            textBox_input.Location = new System.Drawing.Point(5, 6);
-            textBox_input.Name = "textBox_input";
-            textBox_input.Size = new System.Drawing.Size(622, 21);
-            textBox_input.TabIndex = 0;
+            this.textBox_input.Location = new System.Drawing.Point(5, 6);
+            this.textBox_input.Name = "textBox_input";
+            this.textBox_input.Size = new System.Drawing.Size(622, 21);
+            this.textBox_input.TabIndex = 0;
             // 
             // button_input
             // 
             this.button_input.Location = new System.Drawing.Point(642, 6);
             this.button_input.Name = "button_input";
-            this.button_input.Size = new System.Drawing.Size(146, 23);
+            this.button_input.Size = new System.Drawing.Size(146, 74);
             this.button_input.TabIndex = 1;
             this.button_input.Text = "Input";
             this.button_input.UseVisualStyleBackColor = true;
@@ -53,23 +57,59 @@
             // textBox_result
             // 
             this.textBox_result.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.textBox_result.Location = new System.Drawing.Point(5, 33);
+            this.textBox_result.Location = new System.Drawing.Point(5, 86);
             this.textBox_result.Multiline = true;
             this.textBox_result.Name = "textBox_result";
             this.textBox_result.ReadOnly = true;
-            this.textBox_result.Size = new System.Drawing.Size(783, 405);
+            this.textBox_result.Size = new System.Drawing.Size(783, 352);
             this.textBox_result.TabIndex = 2;
+            // 
+            // radioButton_True
+            // 
+            this.radioButton_True.AutoSize = true;
+            this.radioButton_True.Location = new System.Drawing.Point(20, 20);
+            this.radioButton_True.Name = "radioButton_True";
+            this.radioButton_True.Size = new System.Drawing.Size(49, 16);
+            this.radioButton_True.TabIndex = 3;
+            this.radioButton_True.TabStop = true;
+            this.radioButton_True.Text = "True";
+            this.radioButton_True.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_False
+            // 
+            this.radioButton_False.AutoSize = true;
+            this.radioButton_False.Location = new System.Drawing.Point(193, 20);
+            this.radioButton_False.Name = "radioButton_False";
+            this.radioButton_False.Size = new System.Drawing.Size(54, 16);
+            this.radioButton_False.TabIndex = 4;
+            this.radioButton_False.TabStop = true;
+            this.radioButton_False.Text = "False";
+            this.radioButton_False.UseVisualStyleBackColor = true;
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.radioButton_False);
+            this.groupBox.Controls.Add(this.radioButton_True);
+            this.groupBox.Location = new System.Drawing.Point(12, 31);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(313, 49);
+            this.groupBox.TabIndex = 5;
+            this.groupBox.TabStop = false;
+            this.groupBox.Text = "Select";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox);
             this.Controls.Add(this.textBox_result);
             this.Controls.Add(this.button_input);
             this.Controls.Add(textBox_input);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox.ResumeLayout(false);
+            this.groupBox.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -80,6 +120,9 @@
         private System.Windows.Forms.Button button_input;
         private System.Windows.Forms.TextBox textBox_result;
         private System.Windows.Forms.TextBox textBox_input;
+        private System.Windows.Forms.RadioButton radioButton_True;
+        private System.Windows.Forms.RadioButton radioButton_False;
+        private System.Windows.Forms.GroupBox groupBox;
     }
 }
 
--- WindowsFormsApp1_241213/Form1.cs
+++ WindowsFormsApp1_241213/Form1.cs
@@ -13,6 +13,7 @@ namespace WindowsFormsApp1_241213
 {
     public partial class Form1 : Form
     {
+        Random randomObj = new Random();
         public Form1()
         {
             InitializeComponent();
@@ -20,9 +21,77 @@ namespace WindowsFormsApp1_241213
 
         private void button_input_Click(object sender, EventArgs e)
         {
-            // textBox_input.Text의 문자열을
-            // textBox_result.Text로 복사
-            textBox_result.Text = textBox_input.Text;
+            // TextBox_input의 텍스트 값
+            bool my_select;
+
+            if (string.IsNullOrEmpty(textBox_input.Text)) // textBox_input.Text에 값이 없으면, 라디오 버튼을 활용
+            {
+                // TextBox가 비어있으면 라디오 버튼 값을 사용
+                if (radioButton_True.Checked) // True 버튼이 눌려있으면
+                {
+                    my_select = true;
+                    print_result_coin(my_select);
+                }
+                else // 버튼이 2개이고 True 버튼이 눌리지 않으면 False가 눌렸을 것이니까
+                {
+                    my_select = false;
+                    print_result_coin(my_select);
+                }
+            }
+            else // textBox_input.Text에 값이 있으면, 입력값을 활용
+            {
+                if (textBox_input.Text == "True" || textBox_input.Text == "False") // 입력된 값이 True, False 일때만
+                {
+                    my_select = Convert.ToBoolean(textBox_input.Text); // 문자열을 bool 값으로 변경 후 my_select 변수에 복사
+                    print_result_coin(my_select);
+                }
+                else // 이상한 Text가 입력되면 다시 에러 메세지 출력
+                {
+                    textBox_result.Text += "올바른 결과를 입력해주세요.\r\n";
+                }
+                
+            }
+        }
+        string[] coin_top_bottom(bool pick)
+        {
+            int randomValue = randomObj.Next(0, 20); // 0~20 사이의 난수를 생성함
+            int result;
+            string[] result_array = new string[2];
+            // 난수를 0 또는 1로 변환
+            if (randomValue % 2 == 1) // 난수의 나머지가 1이면 즉, 홀수면
+            {
+                result = 1; // 1을 반환함
+            }
+            else // 난수의 나머지가 0이면 즉, 짝수면
+            {
+                result = 0; // 0을 반환함       
+            }
+            result_array[1] = randomValue.ToString();
+            // 사용자 입력과 동전 던지기를 비교하여 결과를 반환함
+            if (((pick == true) & (result == 1)) || ((pick == false) & (result == 0)))
+            {
+                result_array[0] = "승리";
+                return result_array;
+            }
+            else
+            {
+                result_array[0] = "패배";
+                return result_array;
+            }
+        }
+        void print_result_coin(bool select) // 반환할 필요없고 출력만 필요한 함수이니 void 활용
+        {
+            textBox_result.Text += "입력하신 값은 " + select + " 입니다!" + "\r\n";
+            textBox_result.Text += "동전 던지기 결과..." + "\r\n";
+            string[] result_coin = new string[2];
+            result_coin = coin_top_bottom(select);
+            textBox_result.Text += "결과1: " + result_coin[0] + " 동전 랜덤 수: " + result_coin[1] + "\r\n";
+            result_coin = coin_top_bottom(select);
+            textBox_result.Text += "결과2: " + result_coin[0] + " 동전 랜덤 수: " + result_coin[1] + "\r\n";
+            result_coin = coin_top_bottom(select);
+            textBox_result.Text += "결과3: " + result_coin[0] + " 동전 랜덤 수: " + result_coin[1] + "\r\n";
+            result_coin = coin_top_bottom(select);
+            textBox_result.Text += "결과4: " + result_coin[0] + " 동전 랜덤 수: " + result_coin[1] + "\r\n";
         }
     }
 }
