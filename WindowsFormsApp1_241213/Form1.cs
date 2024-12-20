87672cb757f03e0c7dd7b32e1283a4ce0a6c4d4d
dseok2805 <eoierr@gmail.com>
2024-12-20 13:09:34 +0900

    241220 열거형으로 간단하게 만든 가위바위보 게임

--- WindowsFormsApp1_241213/Form1.cs
+++ WindowsFormsApp1_241213/Form1.cs
@@ -14,8 +14,6 @@ namespace WindowsFormsApp1_241213
     public partial class Form1 : Form
     {
         Random randomObj = new Random(); // 랜덤한 점수를 생성하기 위한 함수
-        string user_select;
-        string com_select;
         int com_num;
         int[] score = new int[2];
         public Form1()
@@ -24,38 +22,46 @@ namespace WindowsFormsApp1_241213
             textBox_result.Text = "가위바위보 게임 시작~!\r\n";
         }
 
+        // 가위바위보 열거형 선언
+        enum RockPaperScissors
+        {
+            Rock,   // 0
+            Paper,   // 1
+            Scissors     // 2
+        }
+
+        // 선언한 열거형으로 선택 변수를 선언
+        RockPaperScissors user_select;
+        RockPaperScissors com_select;
+
         private void button_Rock_Click(object sender, EventArgs e)
         {
-            user_select = "바위";
-            com_select = computer_select();
-            RockScissorsPaper_Result(user_select, com_select);
+            PlayGame(RockPaperScissors.Rock);
         }
 
         private void button_Scissors_Click(object sender, EventArgs e)
         {
-            user_select = "가위";
-            com_select = computer_select();
-            RockScissorsPaper_Result(user_select, com_select);
+            PlayGame(RockPaperScissors.Scissors);
         }
 
         private void button_Paper_Click(object sender, EventArgs e)
         {
-            user_select = "보";
-            com_select = computer_select();
-            RockScissorsPaper_Result(user_select, com_select);
+            PlayGame(RockPaperScissors.Paper);
         }
 
-        int[] RockScissorsPaper_Result(string user_select, string com_select)
+        int[] RockScissorsPaper_Result(RockPaperScissors user_select, RockPaperScissors com_select)
         {
             textBox_result.Text += "사용자의 선택: " + user_select + "\r\n";
             textBox_result.Text += "컴퓨터의 선택: " + com_select + "\r\n";
 
-            // 가위바위보 결과 계산
             if (user_select == com_select)
             {
                 textBox_result.Text += "결과: 비겼습니다!\r\n\r\n";
             }
-            else if ((user_select == "가위" && com_select == "보") || (user_select == "바위" && com_select == "가위") || (user_select == "보" && com_select == "바위"))
+            else if (
+                (user_select == RockPaperScissors.Scissors && com_select == RockPaperScissors.Paper) ||
+                (user_select == RockPaperScissors.Rock && com_select == RockPaperScissors.Scissors) ||
+                (user_select == RockPaperScissors.Paper && com_select == RockPaperScissors.Rock))
             {
                 score[0]++;
                 textBox_result.Text += "결과: 사용자가 이겼습니다!\r\n\r\n";
@@ -72,6 +78,8 @@ namespace WindowsFormsApp1_241213
             // 게임 종료 체크
             if (score[0] == 3 || score[1] == 3)
             {
+                textBox_result.Text += "사용자의 선택: " + user_select + "\r\n";
+                textBox_result.Text += "컴퓨터의 선택: " + com_select + "\r\n";
                 string winner = score[0] == 3 ? "사용자" : "컴퓨터";
                 textBox_result.Text += "결과: " + winner + "가 이겼습니다!\r\n\r\n";
                 ResetGame(); // 게임 초기화
@@ -80,33 +88,23 @@ namespace WindowsFormsApp1_241213
 
             return score;
         }
-
-        string computer_select()
-        {
-            com_num = randomObj.Next(0, 3); // 컴퓨터의 선택은 0, 1, 2
-            string computer_result;
-
-            // 컴퓨터의 선택을 string으로 변환
-            if (com_num == 0) // 컴퓨터의 선택이 0이면
-            {
-                computer_result = "바위";
-            }
-            else if (com_num == 1) // 컴퓨터의 선택이 1이면
-            {
-                computer_result = "가위";
-            }
-            else // 컴퓨터의 선택이 2면
-            {
-                computer_result = "보";
-            }
-
-            return computer_result;
-        }
         void ResetGame()
         {
             score[0] = 0;
             score[1] = 0;
             textBox_result.Text += "\r\n게임이 초기화되었습니다!\r\n\r\n";
+        }
+
+        void PlayGame(RockPaperScissors userChoice)
+        {
+            user_select = userChoice;
+            com_select = ComputerSelect();
+            RockScissorsPaper_Result(user_select, com_select);
+        }
+
+        RockPaperScissors ComputerSelect()
+        {
+            return (RockPaperScissors)randomObj.Next(0, 3); // 컴퓨터의 랜덤 수 0, 1, 2를 열거형으로 명시적 형 변환
         }
     }
 }
